61293d8f9e7ffb722323aef64e4b8c9c192006e3
ronny <ronny_migler@mail.ru>
2022-09-10 20:16:05 +0300

    дз с корректировками

--- src/task/Main.java
+++ src/task/Main.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isLeap(2020) ? "Год високосный" : "Год не високосный");
-        //System.out.println(generateInstallationMassage(0, 2020));
+        //System.out.println(isLeap(2026) ? "Год високосный" : "Год не високосный");
+       printInstallationMassage(0, 2022);
         //System.out.println("Потребуется для доставки дней: " + calculateDeliveryDays(10));
         ;
 
@@ -15,16 +15,18 @@ public class Main {
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 
-    public static String generateInstallationMassage(int os, int productionYear) {
+    private static void printInstallationMassage(int os, int productionYear) {
         var allowedOs = new int[]{0, 1};
         if (!(os == 0 || os == 1)) {
             throw new RuntimeException("Устройство не поддерживается");
         }
+
         var currentYear = LocalDate.now().getYear();
         String versionMessage = productionYear < currentYear ? "облегченную " : "";
         String osMassage = os == 0 ? "iOS" : "Android";
 
-        return String.format("Установите %s версию приложения для %s по ссылке", versionMessage, osMassage);
+        System.out.printf("Установите %s версию приложения для %s по ссылке", versionMessage, osMassage);
+
     }
 
     private static int calculateDeliveryDays(int distance) {
